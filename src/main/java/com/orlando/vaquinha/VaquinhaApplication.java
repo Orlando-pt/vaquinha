60b44f1c88369c07c6df4811fd427ddb99bf27a1
Orlando-pt <orlando.macedo15@ua.pt>
2021-08-09 01:45:43 +0100

    add spring files

---
+++ src/main/java/com/orlando/vaquinha/VaquinhaApplication.java
@@ -0,0 +1,13 @@
+package com.orlando.vaquinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaquinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaquinhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orlando/vaquinha/VaquinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlando.vaquinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaquinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
