1ee2fba679fbfc799d04e1c99ee001bc3149a3f7
Orlando-pt <orlando.macedo15@ua.pt>
2021-08-14 18:32:45 +0100

    form with contribution details

---
+++ src/main/java/com/orlando/vaquinha/dto/PaymentDto.java
@@ -0,0 +1,20 @@
+package com.orlando.vaquinha.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Setter @Getter
+@ToString
+public class PaymentDto {
+    
+    private String name;
+
+    private Double amount;
+}
--- src/main/java/com/orlando/vaquinha/web/FrontController.java
+++ src/main/java/com/orlando/vaquinha/web/FrontController.java
@@ -1,11 +1,17 @@
 package com.orlando.vaquinha.web;
 
+import java.sql.Timestamp;
+
+import com.orlando.vaquinha.dto.PaymentDto;
+import com.orlando.vaquinha.model.Payment;
 import com.orlando.vaquinha.paypal.ApiRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +25,39 @@ public class FrontController {
     public String index(Model model) {
         model.addAttribute("balance", paypalApiRequest.getBalance());
         model.addAttribute("transactions", paypalApiRequest.getTransactions().getPaymentTransactions());
+        model.addAttribute("payment", new PaymentDto());
         return "index";
+    }
+
+    @PostMapping("/contribution")
+    public String payment(@ModelAttribute PaymentDto payment, Model model) {
+        System.out.println(payment);
+        if (!verifyPaymentParams(model, payment)) {
+            var contribution = new Payment();
+            contribution.setName(payment.getName());
+            contribution.setAmount(payment.getAmount());
+            contribution.setTimestamp(new Timestamp(System.currentTimeMillis()));
+            System.out.println(contribution);
+        }
+        return "payment";
+    }
+
+    private Boolean verifyPaymentParams(Model model, PaymentDto payment) {
+        if (payment.getAmount() == null || payment.getName() == null) {
+            model.addAttribute("error-message", "Por favor verifique os campos de submissão. Não são aceites campos vazios.");            
+            System.out.println("Null values");
+            return true;
+        }
+        if (payment.getName().isEmpty()) {
+            model.addAttribute("error-message", "Por favor verifique o nome da submissão.");
+            System.out.println("Empty name");
+            return true;
+        }
+        if (Double.compare(payment.getAmount(), Double.valueOf(0.0)) <= 0) {
+            model.addAttribute("error-message", "O valor da contribuição deve ser superior a 0. Por favor verique novamente o campo.");
+            System.out.println("Bad amount value");
+            return true;
+        }
+        return false;
     }
 }
