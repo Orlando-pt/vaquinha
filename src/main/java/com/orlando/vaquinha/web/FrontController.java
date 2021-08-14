7cf0047bd68979ec9a5dd1189f16b1cdc1370f09
Orlando-pt <orlando.macedo15@ua.pt>
2021-08-14 19:03:35 +0100

    add contributions to main page

--- src/main/java/com/orlando/vaquinha/web/FrontController.java
+++ src/main/java/com/orlando/vaquinha/web/FrontController.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 import com.orlando.vaquinha.dto.PaymentDto;
 import com.orlando.vaquinha.model.Payment;
 import com.orlando.vaquinha.paypal.ApiRequest;
+import com.orlando.vaquinha.repository.PaymentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,12 +21,16 @@ public class FrontController {
 
     @Autowired
     private ApiRequest paypalApiRequest;
+
+    @Autowired
+    private PaymentRepository paymentRepository;
     
     @GetMapping(value = {"", "/", "/index"})
     public String index(Model model) {
         model.addAttribute("balance", paypalApiRequest.getBalance());
         model.addAttribute("transactions", paypalApiRequest.getTransactions().getPaymentTransactions());
         model.addAttribute("payment", new PaymentDto());
+        model.addAttribute("contributions", paymentRepository.findAll());
         return "index";
     }
 
@@ -34,9 +39,13 @@ public class FrontController {
         System.out.println(payment);
         if (!verifyPaymentParams(model, payment)) {
             var contribution = new Payment();
+
             contribution.setName(payment.getName());
             contribution.setAmount(payment.getAmount());
             contribution.setTimestamp(new Timestamp(System.currentTimeMillis()));
+
+            paymentRepository.save(contribution);
+
             System.out.println(contribution);
         }
         return "payment";
